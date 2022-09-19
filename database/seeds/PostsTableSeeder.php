a322e2180bb7553648d2fb6202660a0553e88403
Antonius131 <antomendetta@gmail.com>
2022-09-19 15:42:21 +0200

    populating db table with faker

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -9,8 +11,15 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i<50; $i++) { 
+            $newPost = new Post();
+            $newPost->author = $faker->name();
+            $newPost->title = $faker->realText(30);
+            $newPost->article_text = $faker->paragraph(4, true);
+            $newPost->date = $faker->dateTimeThisYear();
+            $newPost->save();
+        }
     }
 }
