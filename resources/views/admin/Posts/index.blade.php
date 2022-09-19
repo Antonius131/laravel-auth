25dfaa1d7cc070021384366739ebb9734a121f97
Antonius131 <antomendetta@gmail.com>
2022-09-19 22:03:36 +0200

    creating new posts index

--- resources/views/admin/Posts/index.blade.php
+++ resources/views/admin/Posts/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>This is index.blabe</h2>
+<div class="container">
+   <div class="row py-5">
+      <div class="col-8 offset-2">
+         <h2>This is posts.index</h2>
+      </div>
+   </div>
+</div>
 @endsection
