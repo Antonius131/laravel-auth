ac4f480212b7171f8db09a42ab523d45f85f6534
Antonius131 <antomendetta@gmail.com>
2022-09-19 22:16:19 +0200

    get posts from db

--- resources/views/admin/Posts/index.blade.php
+++ resources/views/admin/Posts/index.blade.php
@@ -4,7 +4,28 @@
 <div class="container">
    <div class="row py-5">
       <div class="col-8 offset-2">
-         <h2>This is posts.index</h2>
+         <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Author</th>
+                <th scope="col">Title</th>
+                <th scope="col">Date</th>
+              </tr>
+            </thead>
+            <tbody>
+               @forelse ($posts as $post)                  
+                  <tr>
+                     <td>{{ $post->id }}</td>
+                     <td>{{ $post->author }}</td>
+                     <td>{{ $post->title }}</td>
+                     <td>{{ $post->date }}</td>
+                  </tr>
+               @empty
+                  <h2 class="text-center">No posts to show</h2>
+               @endforelse
+            </tbody>
+          </table>
       </div>
    </div>
 </div>
