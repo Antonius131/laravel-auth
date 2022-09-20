8676d0f709e72b124040ad4772c6b0de83ee0557
Antonius131 <antomendetta@gmail.com>
2022-09-20 13:20:55 +0200

    creating new edit.blade

---
+++ resources/views/admin/Posts/edit.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+   <div class="container">
+      <div class="row">
+         <div class="col-8 offset-2">
+            <h2>I am your edit.blade</h2>
+         </div>
+      </div>
+   </div>
+@endsection
