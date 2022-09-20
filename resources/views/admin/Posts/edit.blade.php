c3899f560ccf4facb0a26962ac9423f88ea2e295
Antonius131 <antomendetta@gmail.com>
2022-09-20 14:31:47 +0200

    adding padding to row

--- resources/views/admin/Posts/edit.blade.php
+++ resources/views/admin/Posts/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
    <div class="container">
-      <div class="row">
+      <div class="row py-5">
          <div class="col-8 offset-2">
             <h2>I am your edit.blade</h2>
          </div>
