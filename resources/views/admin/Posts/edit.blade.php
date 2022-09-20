1f7ede555976253da5ab6c540df4060723b4b7a7
Antonius131 <antomendetta@gmail.com>
2022-09-20 14:34:49 +0200

    including form.blade

--- resources/views/admin/Posts/edit.blade.php
+++ resources/views/admin/Posts/edit.blade.php
@@ -4,7 +4,12 @@
    <div class="container">
       <div class="row py-5">
          <div class="col-8 offset-2">
-            <h2>I am your edit.blade</h2>
+            <form action="{{ route('admin.posts.update', $post->id) }}" method="POST">
+               @csrf
+               @method('PUT')
+               @include('admin.Posts.includes.form')
+               <button type="submit" class="btn btn-primary">Edit post</button>
+            </form>
          </div>
       </div>
    </div>
