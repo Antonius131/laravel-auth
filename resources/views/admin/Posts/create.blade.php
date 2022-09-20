a2b7a23e7b574c18c7b90940497e22bd521fb83d
Antonius131 <antomendetta@gmail.com>
2022-09-20 12:11:07 +0200

    including form.blade

--- resources/views/admin/Posts/create.blade.php
+++ resources/views/admin/Posts/create.blade.php
@@ -4,7 +4,11 @@
    <div class="container">
       <div class="row py-5">
          <div class="col-8 offset-2">
-            <h2>I am your create.blade</h2>
+            <form action="{{ route('admin.posts.store') }}" method="POST">
+               @csrf
+               @include('admin.Posts.includes.form')
+               <button type="submit" class="btn btn-primary">Create post</button>
+            </form>
          </div>
       </div>
    </div>
