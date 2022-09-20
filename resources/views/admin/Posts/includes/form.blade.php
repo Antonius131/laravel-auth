ac90dccf5925df38e89cff21a021baa2b5a9e0b3
Antonius131 <antomendetta@gmail.com>
2022-09-20 12:38:01 +0200

    removing input data

--- resources/views/admin/Posts/includes/form.blade.php
+++ resources/views/admin/Posts/includes/form.blade.php
@@ -3,14 +3,10 @@
    <input type="text" name="author" class="form-control" id="input-autor">
 </div>
 <div class="mb-3">
-   <label for="input-title" class="form-label">Author</label>
+   <label for="input-title" class="form-label">Title</label>
    <input type="text" name="title" class="form-control" id="input-title">
 </div>
 <div class="mb-3">
-   <label class="form-label" for="create-textarea">Check me out</label>
-   <textarea name="article_text" class="form-control" id="create-textarea"></textarea>
-</div>
-<div class="mb-3">
-   <label class="form-label" for="input-date">Date</label>
-   <input type="date" name="date" class="form-control" id="input-date">
+   <label class="form-label" for="create-textarea">Post text</label>
+   <textarea name="article_text" class="form-control" id="create-textarea" rows="6"></textarea>
 </div>
